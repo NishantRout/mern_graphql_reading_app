d0d16ed1062aebc482bbd28aa1c033a3df4e8aaa
NishantRout <nishantnarayanrout@gmail.com>
2020-10-30 15:41:48 +0530

    resolve function

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,31 +1,39 @@
-const graphql = require('graphql');
+const graphql = require("graphql");
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
+//dummy data
+const books = [
+  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+];
+
 const BookType = new GraphQLObjectType({
-    name: 'Book',
-    fields: () => ({
-        id: {type: GraphQLString},
-        name: {type: GraphQLString},
-        genre: {type: GraphQLString}
-    })
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
 });
 
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-        book: {
-            type: BookType,
-            args: {
-                id: {type: GraphQLString}
-            },
-            resolve(parent, args){
-                //code to get data from db
-            }
-        }
-    }
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: {
+        id: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        //code to get data from db
+        return books.find((movie) => movie.id == args.id);
+      },
+    },
+  },
 });
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+  query: RootQuery,
 });
