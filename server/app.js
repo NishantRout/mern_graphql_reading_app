ab9c9c5613831bb94249dfb9241c345c92330c36
NishantRout <nishantnarayanrout@gmail.com>
2020-10-30 16:16:12 +0530

    testing queries in graphiql

--- server/app.js
+++ server/app.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema');
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
-    schema: schema
-}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
 
-app.listen('3000', () => {
-    console.log("Now listening for requests on port 3000");
-})
+app.listen("3000", () => {
+  console.log("Now listening for requests on port 3000");
+});
