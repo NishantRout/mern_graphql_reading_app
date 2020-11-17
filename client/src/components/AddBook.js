f84e222861baff6fee914e25d72f6e57bc30b611
NishantRout <nishantnarayanrout@gmail.com>
2020-11-17 18:41:47 +0530

    updating component state

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { getAuthorsQuery } from "../queries/queries";
 
 function AddBook() {
+  const [newBook, setNewBook] = useState({
+    name: "",
+    genre: "",
+    authorId: "",
+  });
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log("📙", newBook);
+  };
   const { loading, error, data } = useQuery(getAuthorsQuery);
   const displayAuthors = () => {
     if (loading) {
@@ -21,18 +30,28 @@ function AddBook() {
   //   console.log("🧔", data);
 
   return (
-    <form id="add-book">
+    <form id="add-book" onSubmit={(e) => submitForm(e)}>
       <div className="field">
         <label>Book name:</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(e) => setNewBook({ ...newBook, name: e.target.value })}
+          value={newBook.name}
+        />
       </div>
       <div className="field">
         <label>Genre:</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(e) => setNewBook({ ...newBook, genre: e.target.value })}
+          value={newBook.genre}
+        />
       </div>
       <div className="field">
         <label>Author:</label>
-        <select>
+        <select
+          onChange={(e) => setNewBook({ ...newBook, authorId: e.target.value })}
+        >
           <option>Select author</option>
           {displayAuthors()}
         </select>
